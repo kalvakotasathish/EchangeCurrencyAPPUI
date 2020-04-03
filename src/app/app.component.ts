3af80e9638289b2ed49887ebc41deb43fa1e17e4
Sathish Kalvakota <Sathish.Kalvakota@lntinfotech.com>
2020-04-03 12:28:04 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient ,HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, Inject, Injectable } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exchange-rate';
-}
+  title = 'exchangeCurrencyApp';
+  constructor(private http: HttpClient) { }
+  //users: Todo[];
+   restItemsUrl = "http://localhost:8080/exchangeService/currentRate";
+ headers:any;
+    result:any;
+    currentExchangeCurrency = false;
+    historyExchangeCurrency=false;
+    historyExchangeCurrencyResult=[];
+     o : any[];
+    dA = [];
+    dates = ['Currency'];
+    rows = [];
+
+  ngOnInit() {
+    
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa("admin" + ':' + "password") });
+
+    this.http.get(this.restItemsUrl,{headers}).subscribe( (data: any) => {
+      if(data!=null){
+        this.result=data.rates;
+      this.currentExchangeCurrency=true;
+
+       
+ }});
+    this.historyCurrency();
+
+    }
+
+    historyCurrency(){
+
+      let historyCurrencyUrl = "http://localhost:8080/exchangeService/historyRates";
+
+      this.http.get(historyCurrencyUrl).subscribe( (data: any) => {
+        if(data!=null){
+
+          this.o  = data.map( el => JSON.parse(el))
+          this.o.forEach( l => {
+          let {rates} = l;
+          this.dates.push(l.date)
+          for(let j in rates) {
+            if(this.dA[j]) {
+              this.dA[j].push(rates[j]);
+              }
+          else {
+            this.dA[j] = [j, rates[j]];
+          }
+      }
+    
+});
+
+    for(let i in this.dA) {
+      this.rows.push(this.dA[i]);
+      }
+          this.historyExchangeCurrency=true;
+          this.currentExchangeCurrency=true;
+        }});
+  
+
+    }
+  }
